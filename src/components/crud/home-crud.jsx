88321994ca032b80d04c740b1235c45ffba1747e
Elbruce4 <78322819+Elbruce4@users.noreply.github.com>
2023-05-09 19:41:39 -0300

    add permissions to crud page

--- src/components/crud/home-crud.jsx
+++ src/components/crud/home-crud.jsx
@@ -1,16 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import "../../styles/crud/home.css"
 
 const HomeCRUD = () => {
 
+    const navigate = useNavigate();
+
+    
+    const isAdmin = localStorage.getItem("isAdmin");
+    console.log("¿Admin?" , isAdmin);
+
+
     return (
         <div>
             CRUD PAGE
-            <div className="div-crud">
-                <button className="crud">CREATE</button>
-                <button className="crud">READ</button>
-                <button className="crud">UPDATE</button>
-                <button className="crud">DELETE</button>
-            </div>
+            {
+                isAdmin == "true" ?
+                <div className="div-crud">
+                    <button className="crud">CREATE</button>
+                    <button className="crud">READ</button>
+                    <button className="crud">UPDATE</button>
+                    <button className="crud">DELETE</button>
+                </div>
+                :
+                <div>
+                    <h3>Upps, no estas autorizado para ver esta página</h3>
+                    <button onClick={() => navigate(-1)}>Volver</button>
+                </div>
+            }
+            
             
         </div>
     );
